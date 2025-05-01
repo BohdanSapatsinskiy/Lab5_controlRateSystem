717fdc8cd0e1a43ccde9445c5cc19f5ac926aa61
BohdanSapatsinskiy <ipzk241_sbi@student.ztu.edu.ua>
2025-05-01 13:47:01 +0300

    Add files via upload

---
+++ controlRateSystem/Views/Shared/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using controlRateSystem.Data.Data;
+
+namespace controlRateSystem.Views.Shared.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly RatesDbContext context;
+
+        public NavigationMenuViewComponent(RatesDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
